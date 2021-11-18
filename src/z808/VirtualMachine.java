e502755055f47aa6f679723c9730227d11414559
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-11-18 17:35:26 -0300

    Functional version

--- src/z808/VirtualMachine.java
+++ src/z808/VirtualMachine.java
@@ -1,5 +1,6 @@
 
-package z808.z808;
+package z808;
+import z808.z808.*;
 
 public class VirtualMachine {
     public static void main(String[] args) {
@@ -19,10 +20,8 @@ public class VirtualMachine {
         Linker ligador = new Linker();
         ligador.link();
 
-        /*
         Z808 proc = new Z808(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Screen fr = new Screen(proc);
         fr.setVisible(true);
-        //*/
     }
 }
